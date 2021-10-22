e94436a3512cc6838c03b174d241601ecbf266bc
YuqinChen <yuqinche@usc.edu>
2021-10-22 13:11:50 -0700

    add java call python function

--- demo/src/main/java/com/example/demo/TrainDataService/TrainDataService.java
+++ demo/src/main/java/com/example/demo/TrainDataService/TrainDataService.java
@@ -4,11 +4,18 @@ import com.example.demo.Model.TrainData;
 import com.example.demo.dao.ImpTrainDataDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.python.util.PythonInterpreter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Service
 public class TrainDataService {
+    public static void main(String[] args) throws Exception {
+        String rlt = testTrain();
+        System.out.println(rlt);
+    }
     private ImpTrainDataDao impTrainDataDao;
     @Autowired
     public TrainDataService(ImpTrainDataDao impTrainDataDao){
@@ -28,6 +35,23 @@ public class TrainDataService {
         //训练过程
         //...
         //返回训练评估值
+        return "there's a 70% chance that this dog will be adopted within 3 days";
+    }
+
+    public static String testTrain() {
+        System.out.println("start run python");
+        try {
+            Process proc = Runtime.getRuntime().exec("python /Users/chenyuqin/Desktop/21_fall_codes_and_relative/dsci551/project/models_manager/xgboost_.py");
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line=null;
+            // Todo: write line to return result, judge if there are multiple lines;
+            while ((line=in.readLine())!=null){
+                System.out.println("Train mse is: "+line);
+            }
+            proc.waitFor();
+        } catch (Exception e) {return "False";}
+        System.out.println("end run python");
+
         return "there's a 70% chance that this dog will be adopted within 3 days";
     }
 
