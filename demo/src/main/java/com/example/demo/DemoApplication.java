74d6211787baca1472d4d5aedd6a3681797521ad
lixingleon <tomatogo@outlook.com>
2021-11-14 14:11:19 -0800

    add simple file upload function

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.Service.FileStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import javax.annotation.Resource;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+	@Resource
+	FileStorageService storageService;
+
+	public static void main(String[] args){
 		SpringApplication.run(DemoApplication.class, args);
+
+	}
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
 	}
 
 }
